75e24dacb3b1dbb1aa236181c2b09ac1a6012d59
Julio Henriquez <julio.m.henriquez@gmail.com>
2017-04-20 22:24:10 -0400

    Refectactor angular2 client out.

--- src/app/components/loading.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'loading',
-  templateUrl: '/components/loading.tpl.html',
-  styleUrls: ['assets/styles/loading.css']
-})
-export class LoadingComponent { }
